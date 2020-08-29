e7af5015d11800ea62726a18d9d926eb444f0792
Kevin Peña <kevin.pena.prog@gmail.com>
2020-08-29 00:05:52 -0500

    Add validation to the regex in the settings dialog

--- src/components/SettingsDialog.js
+++ src/components/SettingsDialog.js
@@ -6,8 +6,11 @@ import { DEFAULT_MSG_REGEX } from '../config';
 const SettingsDialog = ({ show, onClose = () => {} }) => {
     const [regexSetting, setRegexSetting] = useSetting('msg_regex', DEFAULT_MSG_REGEX);
     const [regex, setRegex] = useState(regexSetting);
+    const [regexError, setRegexError] = useState(false);
 
     const handleSave = () => {
+        if (regexError) return;
+
         // Copy local state to settings
         setRegexSetting(regex);
 
@@ -16,16 +19,28 @@ const SettingsDialog = ({ show, onClose = () => {} }) => {
 
     const resetRegex = () => setRegex(DEFAULT_MSG_REGEX);
 
+    const handleRegexChange = event => {
+        const value = event.target.value;
+        try {
+            new RegExp(value);
+            setRegexError(false);
+        } catch (err) {
+            setRegexError(err.message);
+        }
+        setRegex(value);
+    };
+
     return (
         <Dialog open={show} onClose={onClose} maxWidth='md'>
             <DialogTitle>Live Translate Settings</DialogTitle>
             <DialogContent>
                 <TextField
                     label='Regex'
-                    helperText='Customize the messages that get copied into the translation box'
+                    helperText={regexError || 'Customize the messages that get copied into the translation box'}
+                    error={Boolean(regexError)}
                     fullWidth
                     value={regex}
-                    onChange={e => setRegex(e.target.value)}
+                    onChange={handleRegexChange}
                     InputProps={{
                         endAdornment: <>
                             <Button color='primary' onClick={resetRegex}>
